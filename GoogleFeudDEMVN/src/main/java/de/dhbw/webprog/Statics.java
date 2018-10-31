9136430d7425dd58d1818fef0c22a7d84ea50f9e
cedri <cedri@172.16.66.1>
2018-10-31 09:34:50 +0100

    Statics

---
+++ GoogleFeudDEMVN/src/main/java/de/dhbw/webprog/Statics.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dhbw.webprog;
+
+/**
+ *
+ * @author cedri
+ */
+public class Statics{
+
+    private final static String ZEIGE_AUSWAHL_KATEGORIE = "zeigeAuswahlKategorie";
+    private final static String COUNTER_VERSUCHE = "counterVersuche";
+    private final static String COUNTER_RUNDE = "counterRunde";
+    private final static String COUNTER_PUNKTE = "counterPunkte";
+    private final static String KATEGORIE = "kategorie";
+    private final static String ZU_SUCHENDER_STRING = "zuSuchenderString";
+    private final static String VORSCHLAEGE = "vorschlaege";
+    private final static String BESTENLISTE = "bestenliste";
+    private final static String BTN1 = "btn1";
+    private final static String BTN2 = "btn2";
+    private final static String BTN3 = "btn3";
+    private final static String SPIELER_NAME = "spielerName";
+    private final static String EINGABE = "eingabe";
+    private final static String TREFFER_KEY = "trefferKey";
+    private final static String RESET = "reset";
+    private final static String WAS = "was";
+    private final static String WANN = "wann";
+    private final static String WO = "wo";
+
+    public static String getZEIGE_AUSWAHL_KATEGORIE() {
+        return ZEIGE_AUSWAHL_KATEGORIE;
+    }
+
+    public static String getCOUNTER_VERSUCHE() {
+        return COUNTER_VERSUCHE;
+    }
+
+    public static String getCOUNTER_RUNDE() {
+        return COUNTER_RUNDE;
+    }
+
+    public static String getCOUNTER_PUNKTE() {
+        return COUNTER_PUNKTE;
+    }
+
+    public static String getKATEGORIE() {
+        return KATEGORIE;
+    }
+
+    public static String getZU_SUCHENDER_STRING() {
+        return ZU_SUCHENDER_STRING;
+    }
+
+    public static String getVORSCHLAEGE() {
+        return VORSCHLAEGE;
+    }
+
+    public static String getBESTENLISTE() {
+        return BESTENLISTE;
+    }
+
+    public static String getBTN1() {
+        return BTN1;
+    }
+
+    public static String getBTN2() {
+        return BTN2;
+    }
+
+    public static String getBTN3() {
+        return BTN3;
+    }
+
+    public static String getSPIELER_NAME() {
+        return SPIELER_NAME;
+    }
+
+    public static String getEINGABE() {
+        return EINGABE;
+    }
+
+    public static String getTREFFER_KEY() {
+        return TREFFER_KEY;
+    }
+
+    public static String getRESET() {
+        return RESET;
+    }
+
+    public static String getWAS() {
+        return WAS;
+    }
+
+    public static String getWANN() {
+        return WANN;
+    }
+
+    public static String getWO() {
+        return WO;
+    }
+
+}
